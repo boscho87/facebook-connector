265cb7445939a968f7bc869f048a44fb9384c4df
Simon D. Mueller <simon.d.mueller@gmail.com>
2017-12-24 11:51:57 +0100

    added tests config

---
+++ tests.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simon
+ * Date: 24.12.2017
+ * Time: 11:49
+ */
+
+class tests
+{
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simon
+ * Date: 24.12.2017
+ * Time: 11:51
+ */
+
+namespace FBConnectorTest;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    public function test()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
